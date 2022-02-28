1551c64c78444ca0ecfa8afab793cd6fbd603385
smauas <smauas25@gmail.com>
2022-02-28 19:08:07 +0200

    update

---
+++ react-fronted/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useCallback } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
+const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [userId, setUserId] = useState(false);
+
+	const login = useCallback((uid) => {
+		setIsLoggedIn(true);
+		setUserId(uid);
+	}, []);
+
+	const logout = useCallback(() => {
+		setIsLoggedIn(false);
+		setUserId(null);
+	}, []);
+	let routes;
+	if (isLoggedIn) {
+		routes = (
+			<React.Fragment>
+				<Route path="/" element={<Users />} />
+				<Route path="/:userId/places" element={<UserPlaces />} />
+				<Route path="places/new" element={<NewPlace />} />
+				<Route path="/places/:placeId" element={<UpdatePlace />} />
+				<Route path="*" element={<Navigate to="/" />} />
+			</React.Fragment>
+		);
+	} else {
+		routes = (
+			<React.Fragment>
+				<Route path="/" element={<Users />} />
+				<Route path="/:userId/places" element={<UserPlaces />} />
+				<Route path="/auth" element={<Auth />} />
+				<Route path="*" element={<Navigate to="/auth" />} />
+			</React.Fragment>
+		);
+	}
+	return (
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: isLoggedIn,
+				userId: userId,
+				login: login,
+				logout: logout,
+			}}
+		>
+			<BrowserRouter>
+				<MainNavigation />
+				<main>
+					<Routes>{routes}</Routes>
+				</main>
+			</BrowserRouter>
+		</AuthContext.Provider>
+	);
+};
+
+export default App;
